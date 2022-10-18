8d2de11517ab155bdac77af004801394cd1207ff
Lex-G <alexagrim95@gmail.com>
2022-10-17 17:43:28 -0700

    error in functional component corrected

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -49,8 +49,8 @@ const LoggedInView = (props) => {
   );
 };
 
-class Header extends React.Component {
-  render() {
+export const Header = () => {
+
     return (
       <nav
         className="navbar navbar-expand-md navbar-dark"
@@ -68,6 +68,5 @@ class Header extends React.Component {
       </nav>
     );
   }
-}
 
 export default Header;
